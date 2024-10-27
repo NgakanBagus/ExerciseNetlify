af7fcf2e6753673e1abf474bcced1089ebeb4b7e
BAGUS JAYA <anantawijaya994@gmail.com>
2024-10-27 21:06:22 +0800

    first commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Ngakan Putu Bagus Ananta Wijaya" // TODO: replace this
+const studentId = "FS11233324" // TODO: replace this
+
+export { url, studentName, studentId }
